0d7ac9d4ee40b4813c4535a63d26502df7d797b6
Mehran Rahmanzadeh <mehran@longevityintime.org>
2022-08-21 13:41:12 +0430

    optimize

--- scripts/show_gopro.py
+++ scripts/show_gopro.py
@@ -2,8 +2,11 @@
 To get the proper window size and position, fix windows in the pos and then run this command on terminal:
 $ xwininfo
 """
+import time
 
 import cv2
+from imutils.video import FPS
+from imutils.video import WebcamVideoStream
 
 GOPRO_VIDEO_SOURCE = "udp://@:8554"
 FLIR_VIDEO_SOURCE = "/dev/video3"
@@ -15,13 +18,16 @@ WIN_X = 311
 WIN_Y = 34
 
 
-cap = cv2.VideoCapture(GOPRO_VIDEO_SOURCE)
+cap = WebcamVideoStream(src=GOPRO_VIDEO_SOURCE).start()
+time.sleep(1.0)
+fps = FPS().start()
+# cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
 cv2.namedWindow(WIN_NAME, cv2.WINDOW_NORMAL)
 cv2.resizeWindow(WIN_NAME, WIN_WIDTH, WIN_HEIGHT)
 cv2.moveWindow(WIN_NAME, WIN_X, WIN_Y)
 
 while True:
-    _, frame = cap.read()
+    frame = cap.read()
 
     # get the webcam size
     height, width, channels = frame.shape
@@ -39,6 +45,7 @@ while True:
     cv2.imshow(WIN_NAME, resized_cropped)
 
     key = cv2.waitKey(1)
+    fps.update()
 
     if key == ord('q'):
         if ZOOM_RATIO < 50:
